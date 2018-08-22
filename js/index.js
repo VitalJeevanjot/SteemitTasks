b278c8caa047717d9f3659b0d796d6b1b36a4fc6
Jeevanjot <jeevanjotsingh@yandex.com>
2018-08-22 13:28:47 +0530

    Images working
    Images are fixed,Going for other categories

--- js/index.js
+++ js/index.js
@@ -17,10 +17,10 @@ function showDiscussion(val) {
           let imageUrl = JSON.parse(result[i].json_metadata);
           let image = "img/nimg.png";
           if (imageUrl.hasOwnProperty('image')) {
+            console.log("Has property image");
             if (imageUrl.image[0] !== "") {
-              let image = imageUrl.image[0];
-            }
-            else if (imageUrl.image[0] === "") {
+              image = imageUrl.image[0];
+            } else if (imageUrl.image[0] == "") {
               image = "img/nimg.png"
             }
           }
@@ -29,13 +29,50 @@ function showDiscussion(val) {
         <li class='collection-item avatar'> \
           <a href='http://steemit.com" + result[i].url + "' style='color: inherit;'> \
           <img src='" + image + "' alt='' class='circle'> \
-          <span class='title'>" + result[i].root_title + "</span> \
+          <span class='title' style='font-weight:bold;'>" + result[i].root_title + "</span> \
           <p class='truncate'>" + result[i].body + " \
           </p> \
           </a> \
           <br>\
         </li> \
         ");
+        }
+
+      });
+      break;
+
+
+    case 1:
+      $('#heading').html("Development");
+      $(".collection").empty();
+      steem.api.getDiscussionsByCreated({
+        "tag": 'task-development',
+        "limit": 100
+      }, function(err, result) {
+        console.log(err, result);
+        for (let i = 0; i < result.length; i++) {
+          let imageUrl = JSON.parse(result[i].json_metadata);
+          let image = "img/nimg.png";
+          if (imageUrl.hasOwnProperty('image')) {
+            console.log("Has property image");
+            if (imageUrl.image[0] !== "") {
+              image = imageUrl.image[0];
+            } else if (imageUrl.image[0] == "") {
+              image = "img/nimg.png"
+            }
+          }
+
+          $(".collection").append("      \
+      <li class='collection-item avatar'> \
+        <a href='http://steemit.com" + result[i].url + "' style='color: inherit;'> \
+        <img src='" + image + "' alt='' class='circle'> \
+        <span class='title' style='font-weight:bold;'>" + result[i].root_title + "</span> \
+        <p class='truncate'>" + result[i].body + " \
+        </p> \
+        </a> \
+        <br>\
+      </li> \
+      ");
         }
 
       });
